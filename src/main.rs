bc1905d9d6ec6b5c6afbb6f4738da4fd6a3813b9
Ivan Zub <ivan.zub@mendix.com>
2019-10-10 23:26:42 +0200

    Get all displays and try to do something

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,29 @@ fn main() {
     let displays = CGDisplay::active_displays();
     match displays {
         Result::Ok(displays) => {            
+            for id in &displays {
+                println!("Display id is {}", id);
+                let display = CGDisplay::new(*id);
+                let bounds = display.bounds();
+                println!("Serial {}", display.serial_number());
+                println!(
+                    "Display origin is {}:{}",
+                    bounds.origin.x, bounds.origin.y
+                );
+                println!(
+                    "Display size is {}:{}",
+                    bounds.size.width, bounds.size.height
+                );
+                println!("Starting display configuration");
+                let config = display.begin_configuration().unwrap();
+
+                display.change_display_location(config, 110, 110).unwrap();
+                display
+                    .complete_configuration(&config, CGConfigureOption::ConfigurePermanently)
+                    .unwrap();
+                
+                println!("Finished display configuration");
+            }
         }
         Result::Err(err) => panic!(err),
     }
