0ec59dbf6ea1848c6b5dfdbd99e196ad48b34afe
Ivan Zub <ivan.zub@mendix.com>
2019-10-29 21:54:50 +0100

    Small rebranding and cleanup

--- src/main.rs
+++ src/main.rs
@@ -32,36 +32,42 @@ impl DisplayLocation {
 
 type CommandResult = Result<(), Box<dyn Error>>;
 
+const SAVE_COMMAND: &str = "save";
+const RESTORE_COMMAND: &str = "restore";
+
 fn main() {
-    let config = App::new("DisplayKeeper")
-        .version("0.1")
+    let config = App::new("Display util")
+        .version("0.1.0")
         .author("Ivan Z. <zub.ivan@gmail.com>")
         .arg(
-            Arg::with_name("save")
-                .long("save")
-                .conflicts_with("restore")
+            Arg::with_name(SAVE_COMMAND)
+                .long(SAVE_COMMAND)
+                .conflicts_with(RESTORE_COMMAND)
                 .required(true),
         )
         .arg(
-            Arg::with_name("restore")
-                .long("restore")
-                .conflicts_with("save")
+            Arg::with_name(RESTORE_COMMAND)
+                .long(RESTORE_COMMAND)
+                .conflicts_with(SAVE_COMMAND)
                 .required(true),
         )
         .get_matches();
 
-    let save_location = config.is_present("save");
-    let restore_location = config.is_present("restore");
+    let save_is_set = config.is_present(SAVE_COMMAND);
+    let restore_is_set = config.is_present(RESTORE_COMMAND);
 
-    let execution_result = match (save_location, restore_location) {
-        (true, true) => Result::Err(Box::from("Both 'save' and 'restore' options are specified")),
+    let execution_result = match (save_is_set, restore_is_set) {
+        (true, true) => Result::Err(Box::from(format!(
+            "Both '{}' and '{}' options are specified",
+            SAVE_COMMAND, RESTORE_COMMAND
+        ))),
         (false, true) => {
             println!("Restoring configuration");
-            restore("default")
+            save("default")
         }
         (true, false) => {
             println!("Saving current configuration");
-            save("default")
+            restore("default")
         }
         _ => Result::Err(Box::from("Configuration is invalid")),
     };
@@ -110,12 +116,12 @@ fn read_stored_config(config_name: &str) -> Result<Vec<DisplayLocation>, Box<dyn
 }
 
 fn get_config_file_location() -> PathBuf {
-    const DEFAULT_CONFIG_LOCATION: &str = ".displaykeeper";
+    const DEFAULT_CONFIG_NAME: &str = ".displayutil";
 
     let home_dir = home_dir().unwrap();
     let home_dir = home_dir.as_path();
 
-    let config_location: PathBuf = home_dir.join(DEFAULT_CONFIG_LOCATION);
+    let config_location: PathBuf = home_dir.join(DEFAULT_CONFIG_NAME);
     config_location.to_owned()
 }
 
@@ -133,7 +139,7 @@ fn get_active_displays() -> Result<Vec<DisplayLocation>, Box<dyn Error>> {
             Result::Ok(result)
         }
         Result::Err(error_code) => Result::Err(Box::from(format!(
-            "Cannot get dispaly current display configuration. Error code {}.",
+            "Cannot get current display configuration. Error code {}.",
             error_code
         ))),
     }
@@ -152,28 +158,14 @@ fn restore(config_name: &str) -> CommandResult {
                         let display = CGDisplay::new(id);
                         let bounds = display.bounds();
                         let origin = bounds.origin;
-                        println!("Configuring display id: {}", id);
-                        println!("- model number {}", display.model_number());
-                        println!("- unit number {}", display.unit_number());
-                        println!("- vendor number {}", display.vendor_number());
-
-                        println!("- current display origin is {}:{}", origin.x, origin.y);
-                        println!(
-                            "- stored display origin is {}:{}",
-                            display_config.x, display_config.y
-                        );
 
                         if origin.x as i32 == display_config.x
                             && origin.y as i32 == display_config.y
                         {
-                            println!("- already in the right position");
                             continue;
                         }
 
-                        println!("- starting display configuration");
-
                         change_display_location(id, display_config.x, display_config.y);
-                        println!("- finished display configuration");
                     }
                     None => {
                         println!("No stored config exists");
